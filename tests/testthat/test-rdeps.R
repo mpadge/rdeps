9550665060371c81c2a0ca3b2374f6c9da6e30f4
mpadge <mark.padgham@email.com>
2018-10-02 10:53:18 +0200

    manual add cran mirror in tests

--- tests/testthat/test-rdeps.R
+++ tests/testthat/test-rdeps.R
@@ -7,10 +7,15 @@ test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
              identical (Sys.getenv ("TRAVIS"), "true"))
              #identical (Sys.getenv ("APPVEYOR"), "True"))
 
+# This has to be explicitly set here:
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.r-project.org" 
+options(repos=r)
+
 context ('the tests')
 
 test_that ('query-construction', {
-    if (test_all & has_internet)
+    if (test_all)
     {
         val <- get_rdeps ("osmdata")
         expect_true (is.vector (val))
